0f7df970c78b78e232d5c0f5cb09469fb8cc1797
CupeX <dejan.dj@hotmail.com>
2021-06-15 10:05:28 +0200

    new App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -7,19 +8,101 @@ import AddPage from './pages/AddPage';
 import AllStudentsPage from './pages/studentsPage/AllStudentsPage';
 import AddStudentPage from './pages/studentsPage/AddStudentPage';
 import EditStudentsPage from './pages/studentsPage/EditStudentPage';
-// import StudentDetailsPage from './pages/studentsPage/StudentDetailsPage';
 import AllProfessorsPage from './pages/professorsPage/AllProfessorsPage';
 import AddProfessorsPage from './pages/professorsPage/AddProfessorsPage';
 import EditProfessorPage from './pages/professorsPage/EditProfessorPage';
-// import ProfessorDetailsPage from './pages/professorsPage/ProffesorDetailsPage';
 import AllSubjectsPage from './pages/subjectsPage/AllSubjectsPage';
 import AddSubjectPage from './pages/subjectsPage/AddSubjectPage';
 import EditSubjectsPage from './pages/subjectsPage/EditSubjectPage';
-// import SubjectDetailsPage from './pages/subjectsPage/SubjectDetailsPage';
-// import SubjectDetailsTest from './components/subjects/SubjectDetailsTest';
-import LoadingData from './components/comon/LoadingData';
+import StudentDetailsTest from './components/students/StudentDetailsTest';
+import SubjectDetailsTest from './components/subjects/SubjectDetailsTest';
+import ProfessorDetailsTest from './components/professors/ProfessorDetailsTest';
+import db from './components/firebase';
 
 function App() {
+  const [userSubjects, setUserSubjects] = useState({});
+  const [userStudents, setUserStudents] = useState([]);
+  const [userProfessors, setUserProfessors] = useState([]);
+  const [isLoadedStudents, setIsLoadedStudents] = useState(false);
+  const [isLoadedSubjects, setIsLoadedSubjects] = useState(false);
+  const [isLoadedProfessors, setIsLoadedProfessors] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (isLoadedStudents && isLoadedSubjects) {
+      setIsLoaded(true);
+    }
+  }, [isLoadedStudents, isLoadedSubjects, isLoadedProfessors, isLoaded]);
+
+  useEffect(() => {
+    let subjectRef = db.collection('subjects');
+    subjectRef
+      .get()
+      .then(subjects => {
+        const profArray = [];
+        subjects.forEach(subject => {
+          let data = subject.data();
+          let { id } = subject;
+
+          let payload = {
+            id,
+            ...data,
+          };
+          profArray.push(payload);
+        });
+        setUserSubjects(profArray);
+      })
+      .finally(() => {
+        setIsLoadedSubjects(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    let studentRef = db.collection('students');
+    studentRef
+      .get()
+      .then(students => {
+        const studArray = [];
+        students.forEach(student => {
+          let data = student.data();
+          let { id } = student;
+
+          let payload = {
+            id,
+            ...data,
+          };
+          studArray.push(payload);
+        });
+        setUserStudents(studArray);
+      })
+      .finally(() => {
+        setIsLoadedStudents(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    let profRef = db.collection('professors');
+    profRef
+      .get()
+      .then(professors => {
+        const profArray = [];
+        professors.forEach(professor => {
+          let data = professor.data();
+          let { id } = professor;
+
+          let payload = {
+            id,
+            ...data,
+          };
+          profArray.push(payload);
+        });
+        setUserProfessors(profArray);
+      })
+      .finally(() => {
+        setIsLoadedProfessors(true);
+      });
+  }, []);
+
   return (
     <div>
       <MainNavigation />
@@ -40,19 +123,31 @@ function App() {
           <AllProfessorsPage />
         </Route>
         <Route path="/students-input/all-students/details/:id" exact>
-          <LoadingData />
+          <StudentDetailsTest
+            userSubjects={userSubjects}
+            userStudents={userStudents}
+            userProfessors={userProfessors}
+          />
         </Route>
         <Route path="/students-input/all-students/edit/:id" exact>
           <EditStudentsPage />
         </Route>
         <Route path="/students-input/all-subjects/details/:id" exact>
-          <LoadingData />
+          <SubjectDetailsTest
+            userSubjects={userSubjects}
+            userStudents={userStudents}
+            userProfessors={userProfessors}
+          />
         </Route>
         <Route path="/students-input/all-subjects/edit/:id" exact>
           <EditSubjectsPage />
         </Route>
         <Route path="/students-input/all-professors/details/:id" exact>
-          <LoadingData />
+          <ProfessorDetailsTest
+            userSubjects={userSubjects}
+            userStudents={userStudents}
+            userProfessors={userProfessors}
+          />
         </Route>
         <Route path="/students-input/all-professors/edit/:id" exact>
           <EditProfessorPage />
