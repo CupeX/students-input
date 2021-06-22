4d788200043d720a4d42167ac145dc3ef36f2cee
CupeX <dejan.dj@hotmail.com>
2021-06-22 16:39:17 +0200

    provider update

--- src/store/DataProvider.js
+++ src/store/DataProvider.js
@@ -36,56 +36,65 @@ const DataProvider = props => {
 
   const fetchStudents = () => {
     let studentRef = db.collection('students');
-    studentRef.get().then(students => {
-      const studArray = [];
-      students.forEach(student => {
-        let data = student.data();
-        let { id } = student;
+    studentRef
+      .get()
+      .then(students => {
+        const studArray = [];
+        students.forEach(student => {
+          let data = student.data();
+          let { id } = student;
 
-        let payload = {
-          id,
-          ...data,
-        };
-        studArray.push(payload);
-      });
-      setUserStudents(studArray);
-    });
+          let payload = {
+            id,
+            ...data,
+          };
+          studArray.push(payload);
+        });
+        setUserStudents(studArray);
+      })
+      .finally(setIsLoadedStudents(true));
   };
 
   const fetchSubjects = () => {
     let subjectRef = db.collection('subjects');
-    subjectRef.get().then(subjects => {
-      const subArray = [];
-      subjects.forEach(subject => {
-        let data = subject.data();
-        let { id } = subject;
+    subjectRef
+      .get()
+      .then(subjects => {
+        const subArray = [];
+        subjects.forEach(subject => {
+          let data = subject.data();
+          let { id } = subject;
 
-        let payload = {
-          id,
-          ...data,
-        };
-        subArray.push(payload);
-      });
-      setUserSubjects(subArray);
-    });
+          let payload = {
+            id,
+            ...data,
+          };
+          subArray.push(payload);
+        });
+        setUserSubjects(subArray);
+      })
+      .finally(setIsLoadedSubjects(true));
   };
 
   const fetchProfessors = () => {
     let profRef = db.collection('professors');
-    profRef.get().then(professors => {
-      const profArray = [];
-      professors.forEach(professor => {
-        let data = professor.data();
-        let { id } = professor;
+    profRef
+      .get()
+      .then(professors => {
+        const profArray = [];
+        professors.forEach(professor => {
+          let data = professor.data();
+          let { id } = professor;
 
-        let payload = {
-          id,
-          ...data,
-        };
-        profArray.push(payload);
-      });
-      setUserProfessors(profArray);
-    });
+          let payload = {
+            id,
+            ...data,
+          };
+          profArray.push(payload);
+        });
+        setUserProfessors(profArray);
+      })
+      .finally(setIsLoadedProfessors(true));
   };
 
   return (
